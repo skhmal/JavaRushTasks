006dd8fa239efd76e92788c1e82d27728b8a6a73
Sergei Hmal <testingcartman@gmail.com>
2021-10-13 19:18:19 +0200

    first commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
@@ -4,11 +4,18 @@ package com.javarush.task.pro.task10.task1021;
 Автопроизводитель
 */
 
+import java.util.Calendar;
+
 public class Car {
     private String model;
     private int year;
     private String color;
 
+    public void initialize(String model, int year, String color){
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
     //напишите тут ваш код
 
 }
